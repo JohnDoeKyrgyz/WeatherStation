603b47aabf0cb48a567aaed008b2726d26025a5c
John Atwood <JohnDoeKyrgyz@gmail.com>
2017-07-11 09:42:09 -0400

    Adding rudamentary error handling

--- WundergroundForwarder/run.fsx
+++ WundergroundForwarder/run.fsx
@@ -32,33 +32,35 @@ let Run(req: HttpRequestMessage, log: TraceWriter) =
         log.Info("Function invoked")
 
         let! content = req.Content.ReadAsStringAsync() |> Async.AwaitTask
+        if String.IsNullOrWhiteSpace(content) then
+            return req.CreateErrorResponse(HttpStatusCode.BadRequest, "Expected request data")
+        else
+            let payload = Payload.Parse content        
+            let reading = payload.Body
+            let dateUtc = reading.Time.ToUniversalTime().ToString("yyyy-mm-dd hh:mm:ss")
+            let windSpeed = (defaultArg reading.SpeedMetersPerSecond 0.0m) * 2.23694m
+            let windDirection = (defaultArg reading.DirectionSixteenths 0.0m) * (360.0m / 16.0m)
+            let temperature = (defaultArg reading.TemperatureCelciusHydrometer 0.0m) * 9.0m/5.0m  + 32.0m
+            let barometer = (defaultArg reading.PressurePascal 0.0m) * 0.0002953m
 
-        let payload = Payload.Parse content        
-        let reading = payload.Body
-        let dateUtc = reading.Time.ToUniversalTime().ToString("yyyy-mm-dd hh:mm:ss")
-        let windSpeed = (defaultArg reading.SpeedMetersPerSecond 0.0m) * 2.23694m
-        let windDirection = (defaultArg reading.DirectionSixteenths 0.0m) * (360.0m / 16.0m)
-        let temperature = (defaultArg reading.TemperatureCelciusHydrometer 0.0m) * 9.0m/5.0m  + 32.0m
-        let barometer = (defaultArg reading.PressurePascal 0.0m) * 0.0002953m
+            let url = "https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php"
+            let queryParameters = [ 
+                "ID", payload.Body.StationId
+                "PASSWORD", payload.Body.Password
+                "action", "updateraw"
+                "dateutc", dateUtc
+                "realtime", "1"
+                "windspeedmph", string windSpeed
+                "winddir", string windDirection
+                "tempf", string temperature
+                "humidity", defaultArg reading.HumidityPercent 0.0m |> string
+                "baromin", string barometer]
 
-        let url = "https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php"
-        let queryParameters = [ 
-            "ID", payload.Body.StationId
-            "PASSWORD", payload.Body.Password
-            "action", "updateraw"
-            "dateutc", dateUtc
-            "realtime", "1"
-            "windspeedmph", string windSpeed
-            "winddir", string windDirection
-            "tempf", string temperature
-            "humidity", defaultArg reading.HumidityPercent 0.0m |> string
-            "baromin", string barometer]
+            log.Info( sprintf "Wunderground Request: %A" queryParameters )
 
-        log.Info( sprintf "Wunderground Request: %A" queryParameters )
+            let! wundergroundResponse = Http.AsyncRequest( url, queryParameters )
 
-        let! wundergroundResponse = Http.AsyncRequest( url, queryParameters )
+            log.Info( sprintf "Wunderground Response: %A" wundergroundResponse )
 
-        log.Info( sprintf "Wunderground Response: %A" wundergroundResponse )
-
-        return req.CreateResponse(wundergroundResponse)
+            return req.CreateResponse(wundergroundResponse)
     } |> Async.RunSynchronously
