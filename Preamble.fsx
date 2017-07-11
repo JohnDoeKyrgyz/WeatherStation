ea1c4a06987805bc131338fc6af9dddacbf04479
John Atwood <JohnDoeKyrgyz@gmail.com>
2017-07-11 10:28:57 -0400

    Using paket to load Newtonsoft.Json.dll

--- Preamble.fsx
+++ Preamble.fsx
@@ -1,11 +1,12 @@
 #I __SOURCE_DIRECTORY__
+#r @"packages\Newtonsoft.Json\lib\net45\NewtonSoft.Json.dll"
+
 #if INTERACTIVE
 #I @"node_modules\azure-functions-core-tools\bin"
 #r "Microsoft.Azure.Webjobs.Host.dll"
 #r "System.Net.Http.dll"
 #r "System.Net.Http.Formatting.dll"
 #r "System.Web.Http.dll"
-#r "Newtonsoft.Json.dll"
 
 #endif
 
