030131f1689ba326863a39257639474aa79c04f9
John Atwood <JohnDoeKyrgyz@gmail.com>
2018-04-22 12:25:19 -0400

    Tweaking voltage settings

--- DeviceFirmware/SensorTests/ESP8266/BatteryVoltage/BatteryVoltage.ino
+++ DeviceFirmware/SensorTests/ESP8266/BatteryVoltage/BatteryVoltage.ino
@@ -1,3 +1,8 @@
+/**
+ * Battery Voltage Monitor
+ * https://arduinodiy.wordpress.com/2016/12/25/monitoring-lipo-battery-voltage-with-wemos-d1-minibattery-shield-and-thingspeak/
+ * */
+
 void setup()
 {
     Serial.begin(115200);
@@ -12,12 +17,11 @@ void loop()
     digitalWrite(LED_BUILTIN, HIGH);
 	int raw = analogRead(A0);
     float volt = raw / 1023.0;
-    volt = volt * 4.2;
+    volt = volt * 3.3;
 
     printf("Raw = %d, Volt = %f\r\n", raw, volt);
 
-    delay(500);
     digitalWrite(LED_BUILTIN, LOW);
     
-    delay(1000);
+    delay(200);
 }
