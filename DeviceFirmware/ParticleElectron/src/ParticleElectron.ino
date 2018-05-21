b816e2a582fa810baeb053efc2379c32f91388d5
John Atwood <JohnDoeKyrgyz@gmail.com>
2018-05-20 21:30:28 -0400

    Basic firmware compiles

--- DeviceFirmware/ParticleElectron/src/ParticleElectron.ino
+++ DeviceFirmware/ParticleElectron/src/ParticleElectron.ino
@@ -1,58 +1,158 @@
 #include <ArduinoJson.h>
 #include <Arduino.h>
 
-FuelGauge gauge;
+#include <Adafruit_DHT.h>
+#include <LaCrosse_TX23.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+
+#define FIRMWARE_VERSION "1.0"
+
+struct Settings
+{
+    bool brownout;
+    int brownoutMinutes;
+    long sleepTime;
+    int diagnositicCycles;
+};
 
 struct Reading
 {
     float batteryVoltage;
     float panelVoltage;
+    bool anemometerRead;
     float windSpeed;
+    int windDirection;
+    bool dhtRead;
     float dhtTemperature;
-    float bmpTemperature;
     float humidity;
+    bool bmpRead;
+    float bmpTemperature;
     float pressure;
 };
 
+/* Connections */
+#define BAROMETER_CHIP_SELECT 1
+#define DHTPIN 1
+#define ANEMOMETER 1
+#define PANEL_VOLTAGE 1
+#define LED D7 //Builtin LED
+
+/* Sensors */
+#define DHTTYPE DHT22 // DHT 22  (AM2302), AM2321
+DHT dht(DHTPIN, DHTTYPE);
+Adafruit_BMP280 bmp280(BAROMETER_CHIP_SELECT);
+LaCrosse_TX23 laCrosseTX23(ANEMOMETER);
+FuelGauge gauge;
+
+Settings settings;
+#define diagnosticMode settings.diagnositicCycles
+
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
+
+    //Load saved settings;
+
+    if(diagnosticMode)
+    {
+        pinMode(LED, OUTPUT);
+        digitalWrite(LED, HIGH);
+        settings.diagnositicCycles--;
+
+        //save settings;
+    }
+
+    pinMode(PANEL_VOLTAGE, INPUT);
+
+    dht.begin();
+    bmp280.begin();
 }
 
 JsonObject &serialize(Reading *reading)
 {
-    const size_t bufferSize = JSON_OBJECT_SIZE(7);
+    const size_t bufferSize = JSON_OBJECT_SIZE(8);
     DynamicJsonBuffer jsonBuffer(bufferSize);
 
     JsonObject &root = jsonBuffer.createObject();
-    root["batteryVoltage"] = reading->batteryVoltage;
-    root["panelVoltage"] = reading->panelVoltage;
-    root["windSpeed"] = reading->windSpeed;
-    root["dhtTemperature"] = reading->dhtTemperature;
-    root["bmpTemperature"] = reading->bmpTemperature;
-    root["humidity"] = reading->humidity;
-    root["pressure"] = reading->pressure;
+    root["batt_v"] = reading->batteryVoltage;
+    root["panel_v"] = reading->panelVoltage;
+
+    if(reading->bmpRead)
+    {
+        root["temp_bmp_c"] = reading->bmpTemperature;
+        root["press_pas"] = reading->pressure;
+    }
+    if(reading->dhtRead)
+    {
+        root["temp_dht_c"] = reading->dhtTemperature;
+        root["humidity_perc"] = reading->humidity;
+    }
+    if(reading->anemometerRead)
+    {
+        root["wspeed_mps"] = reading->windSpeed;
+        root["wdir_16ths"] = reading->windDirection;
+    }
 
     return root;
+}
+
+void readAnemometer(Reading *reading)
+{
+    if(!(reading->anemometerRead = laCrosseTX23.read(reading->windSpeed, reading->windDirection)))
+    {
+        Serial.println("ERROR: Could not read anemometer");
+    }
+}
+
+void readVoltage(Reading *reading)
+{
+    reading->batteryVoltage = gauge.getVCell();
+    reading->panelVoltage = 1023.0 / (float)analogRead(PANEL_VOLTAGE);
+}
+
+void readDht(Reading *reading)
+{
+    reading->dhtTemperature = dht.getTempCelcius();
+    reading->humidity = dht.getHumidity();
+    if(!(reading->dhtRead = !isnan(reading->dhtTemperature) && !isnan(reading->humidity)))
+    {
+        Serial.println("ERROR: Could not read DHT temp/humidity sensor");
+    }
+}
+
+void readBmp280(Reading *reading)
+{
+    digitalWrite(BAROMETER_CHIP_SELECT, HIGH);
+    reading->bmpTemperature = bmp280.readTemperature();
+    reading->pressure = bmp280.readPressure();
+    digitalWrite(BAROMETER_CHIP_SELECT, LOW);
+    if(!(reading->bmpRead = !isnan(reading->bmpTemperature) && !isnan(reading->pressure)))
+    {
+        Serial.println("ERROR: BMP280 temp/pressure sensor");
+    }
 }
 
 void loop()
 {
     Reading reading;
-    reading.batteryVoltage = gauge.getVCell();
 
-    reading.panelVoltage = 0.0;
-    reading.windSpeed = 0.0;
-    reading.dhtTemperature = 0.0;
-    reading.bmpTemperature = 0.0;
-    reading.humidity = 0.0;
-    reading.pressure = 0.0;
+    readAnemometer(&reading);
+    readVoltage(&reading);
+    readDht(&reading);
+    readBmp280(&reading);
 
-    JsonObject& jsonReading = serialize(&reading);
-    String publishedReading;
-    jsonReading.printTo(publishedReading);
+    JsonObject &jsonReading = serialize(&reading);
+    int publishedReadingLength = jsonReading.measureLength() + 1;
+    char* publishedReading = (char*)malloc(publishedReadingLength);
+    publishedReading[publishedReadingLength] = NULL;
+    jsonReading.printTo(publishedReading, publishedReadingLength);
 
-    Particle.publish("Reading", publishedReading.c_str(), 60, PRIVATE);
+    Particle.publish("Reading", publishedReading, 60, PRIVATE);
 
-    delay(5000);
+    if(diagnosticMode)
+    {
+        digitalWrite(LED, LOW);
+    }
+    System.sleep(settings.sleepTime);
 }
