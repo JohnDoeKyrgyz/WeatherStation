f22cf036b1fbb2e2ff17a66e59b2d9753b170181
John Atwood <john.atwood@casella.com>
2018-06-07 18:12:32 -0400

    Experiment

--- ServerFunctions/WundergroundForwarder/run.fsx
+++ ServerFunctions/WundergroundForwarder/run.fsx
@@ -2,33 +2,22 @@
 #r @"packages\Newtonsoft.Json\lib\net45\Newtonsoft.Json.dll"
 
 #if INTERACTIVE
-#r @"packages\FSharp.Data\lib\net45\FSharp.Data.dll"
 #I @"C:\Users\jatwood\AppData\Roaming\npm\node_modules\azure-functions-core-tools\bin"
-#r "Microsoft.Azure.Webjobs.Host.dll"
-#r "System.Net.Http.dll"
-#r "System.Net.Http.Formatting.dll"
-#r "Microsoft.WindowsAzure.Storage.dll"
-#else
-#r "FSharp.Data"
 #endif
 
+#r "Microsoft.Azure.Webjobs.Host"
 #r "System.Net.Http"
 #r "Microsoft.Azure.WebJobs"
 #r "Microsoft.WindowsAzure.Storage"
 
-#load "../Database.fsx"
-open Database
-
-
-open System
 open System.Linq
-
+open Microsoft.WindowsAzure.Storage.Table
 open Microsoft.Azure.WebJobs.Host
 
 let rec innerMostException (ex : exn) =
     if ex.InnerException <> null then innerMostException ex.InnerException else ex
 
-let Run(eventHubMessage: string, weatherStationsTable: IQueryable<WeatherStation>, readingsTable: IQueryable<Reading>, storedReading : byref<Reading>, log: TraceWriter) =
+let Run(eventHubMessage: string, weatherStationsTable: IQueryable<TableEntity>, readingsTable: IQueryable<TableEntity>, storedReading : byref<TableEntity>, log: TraceWriter) =
     
     let reading =
         async {
