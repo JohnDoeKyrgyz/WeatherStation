1d24e9dca4e31b30658da5a468b397a780b7b397
John Atwood <john.atwood@casella.com>
2018-06-07 18:24:16 -0400

    Experiment

--- ServerFunctions/WundergroundForwarder/run.fsx
+++ ServerFunctions/WundergroundForwarder/run.fsx
@@ -1,43 +1,111 @@
-#I __SOURCE_DIRECTORY__
+#load "../Preamble.fsx"
+#load "../Database.fsx"
+#load "Hologram.fsx"
+#load "Particle.fsx"
+#load "WundergroundPost.fsx"
 
-#if INTERACTIVE
-#I @"C:\Users\jatwood\AppData\Roaming\npm\node_modules\azure-functions-core-tools\bin"
-#I @"..\packages\FSharp.Data\lib\net45\"
-#I @"..\packages\Newtonsoft.Json\lib\net45\"
-#endif
-
-#r "Microsoft.Azure.Webjobs.Host"
-#r "System.Net.Http"
-#r "Microsoft.Azure.WebJobs"
-#r "Microsoft.WindowsAzure.Storage"
-
-#r "Newtonsoft.Json"
-#r "FSharp.Data"
-
+open System
 open System.Linq
-open Microsoft.WindowsAzure.Storage.Table
+
 open Microsoft.Azure.WebJobs.Host
 
-open FSharp.Data
+open Database
+open Model
+open WundergroundPost
 
-[<Literal>]
-let ParticleSample = __SOURCE_DIRECTORY__ + @"\ParticleStatusUpdate.json"
-type ParticlePayload = JsonProvider<ParticleSample, SampleIsList = true>
+let tryParse parser content =
+    try
+        Choice1Of2 (parser content)
+    with ex -> Choice2Of2 ex
+
+let parsers = 
+    [Particle, Particle.parseValues; Hologram, Hologram.parseValues]
 
 let rec innerMostException (ex : exn) =
     if ex.InnerException <> null then innerMostException ex.InnerException else ex
 
-let Run(eventHubMessage: string, weatherStationsTable: IQueryable<TableEntity>, readingsTable: IQueryable<TableEntity>, storedReading : byref<TableEntity>, log: TraceWriter) =
+let Run(eventHubMessage: string, weatherStationsTable: IQueryable<WeatherStation>, readingsTable: IQueryable<Reading>, storedReading : byref<Reading>, log: TraceWriter) =
     
     let reading =
         async {
             log.Info(eventHubMessage)
 
-            let payload = ParticlePayload.Parse eventHubMessage
+            let parseAttempts = [
+                for (key, parser) in parsers do
+                    yield key, (tryParse (parser log) eventHubMessage)]
 
-            log.Info(sprintf "DeviceId %s" payload.DeviceId)
+            let successfulAttempts = [
+                for (key, attempt) in parseAttempts do
+                    match attempt with
+                    | Choice1Of2 result -> yield key, result
+                    | _ -> ()]
 
-            return None }
+            match successfulAttempts with
+            | (deviceType, values) :: _ ->
+
+                log.Info(sprintf "%A" values)
+                let reading = Model.createReading values
+
+                let partitionKey = string deviceType
+                let deviceSerialNumber = reading.SourceDevice
+                log.Info(sprintf "Searching for device %s %s in registry" partitionKey deviceSerialNumber)
+
+                let weatherStations = 
+                    weatherStationsTable
+                        .Where( fun station -> station.PartitionKey = partitionKey && station.RowKey = deviceSerialNumber )
+                        .ToArray()
+
+                if weatherStations.Length <> 1 then failwithf "WeatherStation %s %s is incorrectly provisioned" partitionKey deviceSerialNumber
+                let weatherStation = weatherStations.[0]
+
+                //Find the last ten minutes of readings
+                let lastTenMinutesOfReadings =
+                    let tenMinutesAgo = reading.ReadingTime.Subtract(TimeSpan.FromMinutes(10.0))
+                    query {
+                        for reading in readingsTable do
+                        where (reading.ReadingTime > tenMinutesAgo && reading.SpeedMetersPerSecond.HasValue)
+                        select reading }
+                    |> Seq.toList
+
+                let additionalReadings = 
+                    match lastTenMinutesOfReadings with
+                    | mostRecentWindReading :: _ -> 
+                        [
+                            let gust = 
+                                lastTenMinutesOfReadings 
+                                |> Seq.map (fun reading -> reading.SpeedMetersPerSecond.Value) 
+                                |> Seq.max 
+                            yield GustMetersPerSecond gust
+                            
+                            let secondsSinceLastRun = int (reading.ReadingTime.Subtract(mostRecentWindReading.ReadingTime).TotalSeconds)
+                            yield RefreshInterval secondsSinceLastRun
+
+                            if values |> Seq.exists (fun value -> match value with | SpeedMetersPerSecond _ -> true | _ -> false) |> not then
+                                yield SpeedMetersPerSecond mostRecentWindReading.SpeedMetersPerSecond.Value
+                        ]
+                    | _ -> []
+                log.Info(sprintf "Extrapolated readings %A" additionalReadings)
+
+                let values = values @ additionalReadings                                                                    
+
+                let valuesSeq = values |> Seq.ofList
+                let! wundergroundResponse = postToWunderground weatherStation.WundergroundStationId weatherStation.WundergroundPassword valuesSeq log
+
+                log.Info(sprintf "%A" wundergroundResponse)
+
+                return Some reading
+            | _ -> 
+                log.Info("No values parsed")
+                for (key, attempt) in parseAttempts do
+                    let message =
+                        match attempt with
+                        | Choice2Of2 ex -> 
+                            string (innerMostException ex)
+                        | _ -> "no exception"
+
+                    log.Info(string key)
+                    log.Error(message)
+                return None }
         |> Async.RunSynchronously
 
     match reading with
