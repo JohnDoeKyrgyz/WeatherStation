713fd711fc5f0ab5848bde0bb0bdbb4bb45078f9
John Atwood <john.atwood@casella.com>
2018-06-07 13:56:45 -0400

    Commenting out references when running in production

--- ServerFunctions/Preamble.fsx
+++ ServerFunctions/Preamble.fsx
@@ -2,12 +2,13 @@
 #r @"packages\Newtonsoft.Json\lib\net45\NewtonSoft.Json.dll"
 #r @"packages\FSharp.Data\lib\net45\FSharp.Data.dll"
 
+#if INTERACTIVE
 #I @"C:\Users\jatwood\AppData\Roaming\npm\node_modules\azure-functions-core-tools\bin"
 #r "Microsoft.Azure.Webjobs.Host.dll"
 #r "System.Net.Http.dll"
 #r "System.Net.Http.Formatting.dll"
-#r "System.Web.Http.dll"
 #r "Microsoft.WindowsAzure.Storage.dll"
+#endif
 
 
 #r "System.Net.Http"
