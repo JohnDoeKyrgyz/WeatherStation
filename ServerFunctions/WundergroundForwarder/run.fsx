3311e3c231ead958697be5fdf7c9236e7e541843
John Atwood <john.atwood@casella.com>
2018-06-07 18:19:14 -0400

    Experiment

--- ServerFunctions/WundergroundForwarder/run.fsx
+++ ServerFunctions/WundergroundForwarder/run.fsx
@@ -1,8 +1,9 @@
 #I __SOURCE_DIRECTORY__
-#r @"packages\Newtonsoft.Json\lib\net45\Newtonsoft.Json.dll"
 
 #if INTERACTIVE
 #I @"C:\Users\jatwood\AppData\Roaming\npm\node_modules\azure-functions-core-tools\bin"
+#I @"..\packages\FSharp.Data\lib\net45\"
+#I @"..\packages\Newtonsoft.Json\lib\net45\"
 #endif
 
 #r "Microsoft.Azure.Webjobs.Host"
@@ -10,9 +11,18 @@
 #r "Microsoft.Azure.WebJobs"
 #r "Microsoft.WindowsAzure.Storage"
 
+#r "Newtonsoft.Json"
+#r "FSharp.Data"
+
 open System.Linq
 open Microsoft.WindowsAzure.Storage.Table
 open Microsoft.Azure.WebJobs.Host
+
+open FSharp.Data
+
+[<Literal>]
+let ParticleSample = __SOURCE_DIRECTORY__ + @"\ParticleStatusUpdate.json"
+type ParticlePayload = JsonProvider<ParticleSample, SampleIsList = true>
 
 let rec innerMostException (ex : exn) =
     if ex.InnerException <> null then innerMostException ex.InnerException else ex
@@ -22,6 +32,11 @@ let Run(eventHubMessage: string, weatherStationsTable: IQueryable<TableEntity>, 
     let reading =
         async {
             log.Info(eventHubMessage)
+
+            let payload = ParticlePayload.Parse eventHubMessage
+
+            log.Info(sprintf "DeviceId %s" payload.DeviceId)
+
             return None }
         |> Async.RunSynchronously
 
