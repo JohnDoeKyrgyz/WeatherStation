2f5de3949c6abb6949582119cfc8e6c79b193a95
John Atwood <john.atwood@casella.com>
2018-06-08 12:55:13 -0400

    Removing old test with hardcoded key, and adding query test script

---
+++ ServerFunctions/DataQueryTest.fsx
@@ -0,0 +1,35 @@
+#load "Preamble.fsx"
+#load "Database.fsx"
+
+open System
+
+open Microsoft.WindowsAzure.Storage
+
+open FSharp.Data
+
+open Database
+
+let connect connectionString =
+    let storageAccount = CloudStorageAccount.Parse connectionString
+    storageAccount.CreateCloudTableClient()
+
+[<Literal>]
+let SettingsJson = __SOURCE_DIRECTORY__ + @"\local.settings.json"
+type LocalSettings = JsonProvider< SettingsJson >
+
+let settings = LocalSettings.GetSample()
+let connectionString = settings.ConnectionStrings.WeatherStationStorage.ConnectionString
+
+let connection = connect connectionString
+let readingsTableRef = connection.GetTableReference("Readings")
+let readingsQuery = readingsTableRef.CreateQuery<Reading>()
+
+let recentReadings =
+    let tenMinutesAgo = DateTime.Now.ToUniversalTime().Subtract(TimeSpan.FromMinutes(10.0))
+    query {
+        for reading in readingsQuery do
+        where (reading.ReadingTime > tenMinutesAgo )
+    }
+
+for reading in recentReadings do
+    printfn "%A %A" reading.ReadingTime reading.SpeedMetersPerSecond    
