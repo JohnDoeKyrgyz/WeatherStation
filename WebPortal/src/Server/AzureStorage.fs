184cdfb3227befd820630b552fa8a46230be15b9
John Atwood <JohnDoeKyrgyz@gmail.com>
2018-08-16 22:05:21 -0400

    Attempting to connect with the Azure storage provider

--- WebPortal/src/Server/AzureStorage.fs
+++ WebPortal/src/Server/AzureStorage.fs
@@ -1,2 +1,6 @@
 namespace AzureStorage
 
+open FSharp.Azure.StorageTypeProvider
+
+type AzureTableStorage = AzureTypeProvider<connectionStringName = "AzureStorageConnection", configFileName="App.config">
+
