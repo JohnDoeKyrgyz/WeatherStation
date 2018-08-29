79139451218dd885e8f1da32a7bc9d94cb1e49c2
John Atwood <john.atwood@casella.com>
2018-08-28 21:20:12 -0400

    Adding basic repository

--- Server/DataModel/Repository.fs
+++ Server/DataModel/Repository.fs
@@ -1,10 +1,21 @@
 ﻿namespace WeatherStation
 
 module Repository =
+    open FSharp.Azure.Storage.Table
 
     type IRepository<'TEntity> =
         abstract member GetAll : unit -> Async<'TEntity list>
-        abstract member Find : unit -> Async<'TEntity list>
 
-    type AzureStorageRepository<'TEntity>
+    let runQuery connection tableName query =
+        async {
+            let! data =
+                query
+                |> fromTableAsync connection tableName
+            let result = [for entity, _ in data -> entity]
+            return result
+        }
 
+    type AzureStorageRepository<'TEntity>(connection, tableName) =
+        let runQuery = runQuery connection tableName
+        interface IRepository<'TEntity> with
+            member this.GetAll() = runQuery Query.all<'TEntity>
