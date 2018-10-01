bc5c1117220fd062184b6cf00b71a9829e858548
John Atwood <john.atwood@casella.com>
2018-10-01 10:42:34 -0400

    Fleshing out basic unit tests for connecting to Particle

---
+++ Server/Tests/Server.Tests/Main.fs
@@ -0,0 +1,13 @@
+﻿namespace WeatherStation.Tests.Functions
+
+module Main =
+
+    open Expecto
+    open System
+
+    [<EntryPoint>]
+    let main argv =
+        let result = Tests.runTestsInAssembly defaultConfig argv
+
+        if result = 0 then Console.Beep(440, 500) else Console.Beep(700, 500)
+        result
---
+++ Server/Tests/Server.Tests/ParticleConnectTests.fs
@@ -0,0 +1,22 @@
+﻿namespace WeatherStation.Tests.Server
+module ParticleConnectTests =
+    open System
+    open Expecto
+    open WeatherStation
+
+    [<Tests>]
+    let getTokenTests =
+
+        testList "Integration" [
+            //testCaseAsync "Connect" (async {return ()})
+            
+            testAsync "Connect" {
+                let! connection = WeatherStation.ParticleConnect.connect
+                Expect.isNotNull connection "No connection available"
+
+                let! devices = connection.GetDevicesAsync() |> Async.AwaitTask
+                Expect.isNotNull devices "No devices returned"
+
+                for device in devices do printfn "%s" device.Id
+            }
+        ]
