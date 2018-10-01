dd70d29483c7db874c9c10593062c776084bbafc
John Atwood <john.atwood@casella.com>
2018-10-01 18:12:58 -0400

    Cache the token so that when the server goes down, it can be reloaded.

--- Server/Tests/Server.Tests/Main.fs
+++ Server/Tests/Server.Tests/Main.fs
@@ -10,4 +10,5 @@ module Main =
         let result = Tests.runTestsInAssembly defaultConfig argv
 
         if result = 0 then Console.Beep(440, 500) else Console.Beep(700, 500)
+        
         result
--- Server/WebPortal/src/Server/ParticleConnect.fs
+++ Server/WebPortal/src/Server/ParticleConnect.fs
@@ -1,17 +1,21 @@
 namespace WeatherStation
 module ParticleConnect =
-    
-    open FSharp.Data
-    open System.Net
-    open OtpNet
-    open Particle.SDK
+
     open System
+    open System.IO
+    open System.Net
     open System.Diagnostics
+
+    open FSharp.Data
+    
+    open OtpNet
+    open Particle.SDK    
 
     type Secrets = JsonProvider< @"Secrets.json" >
 
     type MfaResponse = JsonProvider< """{"mfa_token":"b1abb537-7a9a-4bee-ba3a-10faef51a093","error":"mfa_required","error_description":"Multi-factor authentication required"}""">
-    type TokenResponse = JsonProvider< """{"token_type": "bearer","access_token": "7ac571640bc5625b3d67afe01d245fecb7b70be9","expires_in": 7776000,"refresh_token": "77f45105ee6bdd111ea18cdd1cc479d70ebd3ece"}""" >
+    type TokenResponse = JsonProvider< """{"token_type": "bearer","access_token": "7ac571640bc5625b3d67afe01d245fecb7b70be9","expires_in": 7776000,"refresh_token": "77f45105ee6bdd111ea18cdd1cc479d70ebd3ece"}""">
+    type Token = JsonProvider< """{"token_type": "bearer","access_token": "7ac571640bc5625b3d67afe01d245fecb7b70be9","expires_in": 7776000,"refresh_token": "77f45105ee6bdd111ea18cdd1cc479d70ebd3ece", "expires": "2018-10-01T21:12:20.492Z"}""">
 
     type GetTokenResult = 
         | Token of TokenResponse.Root
@@ -83,32 +87,51 @@ module ParticleConnect =
         let secrets = Secrets.GetSample()
         getTokenWithMfa secrets (generateOtp secrets.ParticleAccountSecret)
 
+    let private savedTokenFile = "ParticleToken.json"
+
+    let savedToken =
+        if File.Exists savedTokenFile
+        then Some (TokenResponse.AsyncLoad savedTokenFile)
+        else None
+
+    let saveToken (token : Token.Root) =
+        async {
+            use output = File.OpenWrite savedTokenFile
+            use outputWriter = new StreamWriter(output)
+            token.JsonValue.WriteTo(outputWriter, JsonSaveOptions.None)
+            do! outputWriter.FlushAsync() |> Async.AwaitTask
+        }
+
+    let expiration (token : TokenResponse.Root) = DateTime.Now.AddSeconds(float token.ExpiresIn)
+    let applyExpiration (token : TokenResponse.Root) = new Token.Root(token.TokenType, token.AccessToken, token.ExpiresIn, token.RefreshToken, expiration token)
+
     let private tokenCache secrets otpGenerator =
 
         let getNewToken =
             async {
-                let! token = getTokenWithMfa secrets (otpGenerator())
-                let expires = DateTime.Now.AddSeconds(float token.ExpiresIn)
-                Debug.WriteLine(sprintf "New token generated. Expires at %A" expires)
-                return expires, token
+                let! getTokenResponse = getTokenWithMfa secrets (otpGenerator())
+                let token = applyExpiration getTokenResponse
+                do! saveToken token
+                Debug.WriteLine(sprintf "New token generated. Expires at %A" token.Expires)                
+                return token
             }
 
         let rec cache token (mailBox : MailboxProcessor<AsyncReplyChannel<string>>) =
             async {
                 let! responseChannel = mailBox.Receive()
-                let! (expiration, token) =
+                let! token =
                     match token with
-                    | Some (expiration, token : TokenResponse.Root) ->
-                        if expiration > DateTime.Now
-                        then async { return expiration, token }
+                    | Some (token : Token.Root) ->                        
+                        if token.Expires > DateTime.Now
+                        then async { return token }
                         else
                             Debug.WriteLine(sprintf "Token expired on %A" expiration)
                             getNewToken
-                    | None -> getNewToken
+                    | None -> getNewToken                        
 
                 responseChannel.Reply token.AccessToken
 
-                do! cache (Some (expiration, token)) mailBox}
+                do! cache (Some token) mailBox}
 
         let mailBox = MailboxProcessor.Start (cache None)
 
