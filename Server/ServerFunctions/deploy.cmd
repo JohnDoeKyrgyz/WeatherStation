3ef805836d81735c7bd91d798baaebd4e1532e74
John Atwood <john.atwood@casella.com>
2018-11-11 08:45:52 -0500

    Fixing issues with deployment

--- Server/ServerFunctions/deploy.cmd
+++ Server/ServerFunctions/deploy.cmd
@@ -3,3 +3,7 @@ dotnet build Server\ServerFunctions\extensions.csproj
 
 dotnet restore Server\ServerFunctions\WeatherStationFunctions.fsproj
 dotnet build Server\ServerFunctions\WeatherStationFunctions.fsproj
+
+dotnet restore Server\Tests\WeatherStationFunctions.Tests\WeatherStationFunctions.Tests.fsproj
+dotnet build Server\Tests\WeatherStationFunctions.Tests\WeatherStationFunctions.Tests.fsproj
+dotnet run --project Server\Tests\WeatherStationFunctions.Tests\WeatherStationFunctions.Tests.fsproj
