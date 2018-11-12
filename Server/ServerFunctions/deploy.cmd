13c12bfc2a55e4830dab09091799523754270884
John Atwood <john.atwood@casella.com>
2018-11-11 19:51:45 -0500

    Attempting to get it to recognize the function

--- Server/ServerFunctions/deploy.cmd
+++
@@ -1,9 +0,0 @@
-dotnet restore Server\ServerFunctions\extensions.csproj
-dotnet build Server\ServerFunctions\extensions.csproj
-
-dotnet restore Server\ServerFunctions\WeatherStationFunctions.fsproj
-dotnet build Server\ServerFunctions\WeatherStationFunctions.fsproj
-
-rem dotnet restore Server\Tests\WeatherStationFunctions.Tests\WeatherStationFunctions.Tests.fsproj
-rem dotnet build Server\Tests\WeatherStationFunctions.Tests\WeatherStationFunctions.Tests.fsproj
-rem dotnet run --project Server\Tests\WeatherStationFunctions.Tests\WeatherStationFunctions.Tests.fsproj
