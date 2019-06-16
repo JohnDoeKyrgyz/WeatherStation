eb6232263b9abd9ae2258058d6cd80f1a37c9716
John Atwood <JohnDoeKyrgyz@gmail.com>
2019-06-16 12:22:53 -0400

    Remove global.json so that we can actually target the right version of .net core

--- Server/build.cmd
+++ Server/build.cmd
@@ -13,7 +13,7 @@ if errorlevel 1 (
 
 dotnet clean
 dotnet build
-"C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe"
+"C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe" start
 dotnet run --project Tests\WeatherStationFunctions.Tests\WeatherStationFunctions.Tests.fsproj
 dotnet run --project Tests\Server.Tests\Server.Tests.fsproj
 
