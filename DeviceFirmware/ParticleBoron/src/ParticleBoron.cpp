ac910d4fd0ca54a3b6bd3674712e34e0a2af8a83
John Atwood <JohnDoeKyrgyz@gmail.com>
2019-10-03 23:29:04 -0400

    It compiles

--- DeviceFirmware/ParticleBoron/src/ParticleBoron.cpp
+++ DeviceFirmware/ParticleBoron/src/ParticleBoron.cpp
@@ -6,16 +6,16 @@
 #line 1 "c:/working/WeatherStation/DeviceFirmware/ParticleBoron/src/ParticleBoron.ino"
 
 void waitForConnection();
-void publishStatusMessage(const char* message);
+void publishStatusMessage(const char *message);
 void onError(const char *message);
 void watchDogTimeout();
 void deepSleep(unsigned long seconds);
 void onSettingsUpdate(const char *event, const char *data);
 void setup();
-bool checkBrownout();
+void checkBrownout();
 void connect();
+bool initializeSensors();
 bool selfTest();
-void initializeSensors();
 void loop();
 #line 2 "c:/working/WeatherStation/DeviceFirmware/ParticleBoron/src/ParticleBoron.ino"
 #define RBG_NOTIFICATIONS_OFF
@@ -38,75 +38,53 @@ SYSTEM_THREAD(ENABLED);
 SYSTEM_MODE(MANUAL);
 
 #include <Wire.h>
-#include "Compass.h"
 #include "settings.h"
+#include "Sensor.h"
 
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-#include <LaCrosse_TX23.h>
-#include <ArduinoJson.h>
-#include <Adafruit_INA219.h>
-
-Adafruit_BME280 bme280;
-LaCrosse_TX23 laCrosseTX23(ANEMOMETER);
-Adafruit_INA219 powerMonitor;
-FuelGauge fuelGuage;
-Compass compassSensor;
 PMIC pmic;
+FuelGauge fuelGuage;
 
 Settings settings;
 unsigned long duration;
 
-struct Reading
-{
-  unsigned int version;
-  float batteryVoltage;
-  float batteryPercentage;
-  bool panelRead;
-  float panelVoltage;
-  float panelCurrent;
-  bool anemometerRead;
-  float windSpeed;
-  int windDirection;
-  bool bmeRead;
-  float bmeTemperature;
-  float pressure;
-  float bmeHumidity;
-  bool compassRead;
-  CompassReading compassReading;
-};
-
-Reading initialReading;
 char messageBuffer[255];
 char statusBuffer[255];
-float systemSoC;
+bool panelOn = true;
 bool firstLoop = true;
+
+CompassSensor compassSensor;
+BatteryPower battery;
+PanelPower panel;
+Anemometer anemometer(ANEMOMETER, ANEMOMETER_TRIES);
+Barometer barometer;
+Sensor *sensors[] = {&panel, &barometer, &anemometer, &battery, &compassSensor};
 
 void waitForConnection()
 {
-  if(!Particle.connected)
+  if (!Particle.connected)
   {
     Serial.print("Waiting for connection...");
     waitUntil(Particle.connected);
-    Serial.println("!");    
+    Serial.println("!");
   }
   Particle.process();
 }
 
-void publishStatusMessage(const char* message){  
+void publishStatusMessage(const char *message)
+{
   Serial.print(message);
   Serial.print(" ");
   waitForConnection();
   int tries = 0;
   bool result;
-  while(!(result = Particle.publish("Status", message, 60, PRIVATE, WITH_ACK)) && ++tries < SEND_TRIES)
+  while (!(result = Particle.publish("Status", message, 60, PRIVATE, WITH_ACK)) && ++tries < SEND_TRIES)
   {
     Serial.print(".");
     delay(3000);
     Particle.process();
   }
   Serial.println();
-  if(!result)
+  if (!result)
   {
     Serial.println("ERROR: Could not publish status message");
   }
@@ -116,56 +94,6 @@ void onError(const char *message)
 {
   RGB.color(255, 255, 0);
   publishStatusMessage(message);
-}
-
-bool readAnemometer(Reading *reading)
-{
-  bool read = false;
-  int tries = 0;
-  while (!(read = laCrosseTX23.read(reading->windSpeed, reading->windDirection)) && tries++ < ANEMOMETER_TRIES)
-  {
-    Serial.print(".");
-  }
-  if (tries > 0)
-  {
-    Serial.println();
-  }
-  if(read)
-  {
-    Serial.printlnf("Wind Speed = %f", reading->windSpeed);
-    Serial.printlnf("Wind Direction = %d", reading->windDirection);
-  }    
-  return read;
-}
-
-bool readVoltage(Reading *reading)
-{
-  reading->batteryVoltage = fuelGuage.getVCell();
-  Serial.printlnf("Battery Voltage = %f", reading->batteryVoltage);
-
-  reading->batteryPercentage = fuelGuage.getSoC();
-  Serial.printlnf("Battery Percentage = %f", reading->batteryPercentage);
-  
-  reading->panelVoltage = powerMonitor.getBusVoltage_V();
-  reading->panelCurrent = powerMonitor.getCurrent_mA();
-  
-  Serial.printlnf("Panel Voltage = %f", reading->panelVoltage);
-  Serial.printlnf("Panel Current = %f", reading->panelCurrent);
-  return reading->panelVoltage < 16;
-}
-
-bool readBme280(Reading *reading)
-{
-  reading->bmeTemperature = bme280.readTemperature();
-  reading->pressure = bme280.readPressure();
-  reading->bmeHumidity = bme280.readHumidity();
-  return !isnan(reading->bmeTemperature) && !isnan(reading->pressure) && reading->pressure > 0 && !isnan(reading->bmeHumidity);
-}
-
-bool readCompass(Reading *reading)
-{
-  reading->compassReading = compassSensor.getReading();
-  return true;
 }
 
 void watchDogTimeout()
@@ -186,7 +114,7 @@ void deepSleep(unsigned long seconds)
   Serial.flush();
   fuelGuage.sleep();
 
-  if(seconds > 360)
+  if (seconds > 360)
   {
     System.sleep({}, RISING, seconds);
   }
@@ -214,48 +142,19 @@ void onSettingsUpdate(const char *event, const char *data)
   publishStatusMessage(buffer);
 }
 
-char *serialize(Reading *reading)
-{
-  char *buffer = messageBuffer;
-  buffer += 
-    sprintf(
-      buffer, 
-      "%df%f:%f", 
-      reading->version, 
-      reading->batteryVoltage, 
-      reading->batteryPercentage);
-  if(reading->panelRead)
-  {
-    buffer += sprintf(buffer, "p%f:%f", reading->panelVoltage, reading->panelCurrent);
-  }   
-  if (reading->bmeRead)  
-  {
-    buffer += sprintf(buffer, "b%f:%f:%f", reading->bmeTemperature, reading->pressure, reading->bmeHumidity);
-  }
-  if (reading->anemometerRead)
-  {
-    buffer += sprintf(buffer, "a%f:%d", reading->windSpeed, reading->windDirection);
-  }
-  if (reading->compassRead)
-  {
-    buffer += sprintf(buffer, "c%f:%f:%f", reading->compassReading.x, reading->compassReading.y, reading->compassReading.z);
-  }
-  return messageBuffer;
-}
-
 void setup()
 {
   //Allow the PMIC to charge the battery from a solar panel
   pmic.begin();
-  pmic.setInputVoltageLimit(5080);  //  for 6V Solar Panels
-  pmic.setInputCurrentLimit(2000) ; // 2000 mA, higher than req'd
-  pmic.setChargeVoltage(4208);      //  Set Li-Po charge termination voltage to 4.21V,  Monitor the Enclosure Temps
+  pmic.setInputVoltageLimit(5080);         //  for 6V Solar Panels
+  pmic.setInputCurrentLimit(2000);         // 2000 mA, higher than req'd
+  pmic.setChargeVoltage(4208);             //  Set Li-Po charge termination voltage to 4.21V,  Monitor the Enclosure Temps
   pmic.setChargeCurrent(0, 0, 1, 1, 1, 0); // 1408 mA [0+0+512mA+256mA+128mA+0] + 512 Offset
   pmic.enableDPDM();
 
   Particle.subscribe("Settings", onSettingsUpdate, MY_DEVICES);
-  
-  Serial.begin(115200); 
+
+  Serial.begin(115200);
   Serial.printlnf("WeatherStation %s", FIRMWARE_VERSION);
 
   //Load saved settings;
@@ -272,15 +171,41 @@ void setup()
   digitalWrite(BUZZER, LOW);
 }
 
-bool checkBrownout()
+void checkBrownout()
 {
   Serial.print("Checking brownout...");
   fuelGuage.begin();
-  systemSoC = fuelGuage.getSoC();
+  float systemSoC = fuelGuage.getSoC();
   bool brownout = settings.brownout && systemSoC < settings.brownoutPercentage;
   Serial.println("!");
   Serial.flush();
-  return brownout;
+
+  if (brownout)
+  {
+    Serial.printlnf("Brownout threshold %f exceeded by system battery percentage %f", settings.brownoutPercentage, systemSoC);
+
+    if (Particle.connected())
+    {
+      Particle.process();
+
+      char *buffer = statusBuffer;
+      sprintf(buffer, "BROWNOUT %f:%d", systemSoC, settings.brownoutMinutes);
+      publishStatusMessage(buffer);
+    }
+
+    //signal the LED red and white
+    RGB.control(true);
+    for (int i = 0; i < 4; i++)
+    {
+      RGB.color(255, 0, 0); //red
+      delay(100);
+      RGB.color(255, 255, 255); //white
+      delay(100);
+    }
+    RGB.control(false);
+
+    deepSleep(settings.brownoutMinutes * 60);
+  }
 }
 
 void connect()
@@ -293,26 +218,31 @@ void connect()
   Particle.process();
 }
 
+bool initializeSensors()
+{
+  bool result = true;
+  for (auto sensor : sensors)
+    result &= sensor->begin();
+  return result;
+}
+
 bool selfTest()
 {
-  Reading reading;
-
   Serial.println("Self Test...");
-  bool anemometer = readAnemometer(&reading);
-  Serial.printlnf("Anemometer %d", anemometer);
 
-  bool bme280 = readBme280(&reading);
-  Serial.printlnf("Temp / Humidity %d", bme280);
+  bool result = true;
+  char *buffer = messageBuffer;
+  for (auto sensor : sensors)
+  {
+    bool success = sensor->getReading(buffer);
+    if (!success)
+    {
+      Serial.printlnf("Could not read %s", sensor->Name);
+    }
+    result &= success;
+  }
 
-  bool compass = readCompass(&reading);
-  Serial.printlnf("Compass %d", compass);
-
-  bool panelPower = readVoltage(&reading);
-  Serial.printlnf("Power %d", panelPower);
-
-  bool result = anemometer && bme280 && compass && panelPower;
-
-  if(result)
+  if (result)
   {
     Serial.println("Self test SUCCESS\n");
 
@@ -328,7 +258,7 @@ bool selfTest()
     Serial.println("Self test FAIL\n");
 
     //three short beeps
-    for(int i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
       pinMode(BUZZER, OUTPUT);
       digitalWrite(BUZZER, HIGH);
@@ -342,192 +272,124 @@ bool selfTest()
   return result;
 }
 
-void initializeSensors()
-{
-  Serial.print("Initializing sensors...");    
-  powerMonitor.begin();
-  powerMonitor.setCalibration_16V_400mA();
-  bme280.begin(0x76);
-  compassSensor.begin();
-  Serial.println("!");
-}
-
 void loop()
 {
   duration = millis();
   ApplicationWatchdog watchDog = ApplicationWatchdog(WATCHDOG_TIMEOUT, watchDogTimeout);
 
-  if(checkBrownout()) 
-  {
-    Serial.printlnf("Brownout threshold %f exceeded by system battery percentage %f", settings.brownoutPercentage, systemSoC);
-    
-    //signal the LED red and white
-    RGB.control(true);
-    for(int i = 0; i < 4; i++)
-    {
-      RGB.color(255, 0, 0); //red
-      delay(100);
-      RGB.color(255, 255, 255); //white
-      delay(100);
-    }
-    RGB.control(false);
+  checkBrownout();
 
-    deepSleep(settings.brownoutMinutes * 60);
+  connect();
+
+  //signal LED if in Diagnostic Mode
+  if (settings.diagnositicCycles > 0)
+  {
+    pinMode(LED, OUTPUT);
+    digitalWrite(LED, HIGH);
+    settings.diagnositicCycles = settings.diagnositicCycles - 1;
+    saveSettings(settings);
   }
-  else
+
+  digitalWrite(PERIPHERAL_POWER, HIGH);
+
+  if (initializeSensors())
   {
-    connect();
-    
-    //signal LED if in Diagnostic Mode
-    if (settings.diagnositicCycles > 0)
+    if (firstLoop)
     {
-      pinMode(LED, OUTPUT);
-      digitalWrite(LED, HIGH);
-      settings.diagnositicCycles = settings.diagnositicCycles - 1;
-      saveSettings(settings);
-    }
-
-    digitalWrite(PERIPHERAL_POWER, HIGH);
-
-    initializeSensors();
-
-    if(firstLoop)
-    {
-      selfTest();
-
-      publishStatusMessage("START");
+      if (selfTest())
+      {
+        publishStatusMessage("START");
+      }
+      else
+      {
+        onError("SELF TEST FAIL");
+      }
       firstLoop = false;
     }
 
-    //take an initial wind reading
-    if (!(initialReading.anemometerRead = readAnemometer(&initialReading)))
+    char *buffer = messageBuffer;
+    int sensorCount = sizeof(sensors) / sizeof(sensors[0]);
+    bool results[sensorCount];
+    for (int i = 0; i < sensorCount; i++)
     {
-      onError("ERROR: Could not get initial wind reading");
-    }  
-  
-    Reading reading;
-    reading.version = settings.version;
-
-    Serial.println("Reading...");
-    int tries = 0;
-    while(!(reading.panelRead = readVoltage(&reading)) && tries++ < POWER_MONITOR_TRIES)
-    {
-      digitalWrite(PERIPHERAL_POWER, LOW);
-      delay(100);
-      digitalWrite(PERIPHERAL_POWER, HIGH);
-      delay(100);
-      Wire.reset();
-      delay(100);
-      initializeSensors();
+      Sensor *sensor = sensors[i];
+      results[i] = sensor->getReading(buffer);
     }
 
-    if(!reading.panelRead)
+    for (int i = 0; i < sensorCount; i++)
     {
-      onError("ERROR: INA219 power monitor sensor");
-    }
-
-    if (!(reading.bmeRead = readBme280(&reading)))
-    {
-      onError("ERROR: BME280 temp/pressure sensor");
-    }
-    if (!(reading.compassRead = readCompass(&reading)))
-    {
-      onError("ERROR: Could not read compass");
-    }
-    if (!(reading.anemometerRead = readAnemometer(&reading)))
-    {
-      onError("ERROR: Could not read anemometer");
-    }
-
-    digitalWrite(PERIPHERAL_POWER, LOW);
-    
-    //take the greater of the initial wind reading or the most recent wind reading
-    if (!reading.anemometerRead || (reading.anemometerRead && initialReading.anemometerRead && initialReading.windSpeed > reading.windSpeed))
-    {
-      reading.windSpeed = initialReading.windSpeed;
-      reading.windDirection = initialReading.windDirection;
-      Serial.print("Using faster wind speed ");
-      Serial.print(initialReading.windSpeed);
-      Serial.print(", ");
-      Serial.println(reading.windSpeed);
-    }
-
-    //send serialized reading to the cloud
-    char *publishedReading = serialize(&reading);
-    Serial.println(publishedReading);
-
-    waitForConnection();
-
-    tries = SEND_TRIES;
-    bool sentReading = false;
-    do
-    {
-      watchDog.checkin();    
-      Serial.print("Sending reading... ");
-      sentReading = Particle.publish("Reading", publishedReading, 60, PRIVATE, WITH_ACK);
-      Serial.print(".");
-    }
-    while(!sentReading && --tries > 0);
-
-    Serial.printlnf(" %s!", sentReading ? "+" : "-");    
-
-    Serial.printlnf("DIAGNOSTIC COUNT %d", settings.diagnositicCycles);
-    digitalWrite(LED, LOW);
-
-    //Check for a brownout condition after sending the reading. The connection to the cell tower will be enabled, and we can send a message
-    if(checkBrownout()) 
-    {
-      Serial.printlnf("Brownout threshold %f exceeded by system battery percentage %f", settings.brownoutPercentage, systemSoC);
-      Particle.process();
-
-      char *buffer = statusBuffer;
-      sprintf(buffer, "BROWNOUT %f:%d", systemSoC, settings.brownoutMinutes);
-      publishStatusMessage(buffer);
-
-      deepSleep(settings.brownoutMinutes * 60);
-    }
-
-    //Publish a message if the panel starts or stops charging the battery
-    if(reading.panelRead)
-    {
-      bool charging = 
-        reading.panelCurrent >= CHARGE_CURRENT_LOW_THRESHOLD
-        && reading.panelCurrent <= CHARGE_CURRENT_HIGH_THRESHOLD;
-      if(!charging)
+      if (!results[i])
       {
-        publishStatusMessage("PANEL OFF");
-        if(settings.panelOffMinutes > 0)
-        {
-          deepSleep(settings.panelOffMinutes * 60);
-        }
+        sprintf(statusBuffer, "ERROR: %s", sensors[i]->Name);
+        onError(statusBuffer);
       }
-    }    
-
-    //sleep till the next reading
-    void (*sleepAction)();
-    const char *sleepMessage;
-    if (settings.useDeepSleep)
-    {
-      sleepMessage = "DEEP";
-      sleepAction = []() {
-        deepSleep(settings.sleepTime);
-      };
     }
-    else
-    {
-      sleepMessage = "LIGHT";
-      sleepAction = []() {
-        delay(settings.sleepTime);
-        setup();
-      };
-    }
-
-    Serial.printlnf("%s SLEEP %d", sleepMessage, settings.sleepTime);
-    Serial.printlnf("DURATION %d", millis() - duration);
-
-    Particle.process();
-    Serial.flush();
-
-    sleepAction();
   }
+
+  digitalWrite(PERIPHERAL_POWER, LOW);
+
+  //send serialized reading to the cloud
+  waitForConnection();
+
+  int tries = SEND_TRIES;
+  bool sentReading = false;
+  do
+  {
+    watchDog.checkin();
+    Serial.print("Sending reading... ");
+    sentReading = Particle.publish("Reading", messageBuffer, 60, PRIVATE, WITH_ACK);
+    Serial.print(".");
+  } while (!sentReading && --tries > 0);
+
+  Serial.printlnf(" %s!", sentReading ? "+" : "-");
+
+  Serial.printlnf("DIAGNOSTIC COUNT %d", settings.diagnositicCycles);
+  digitalWrite(LED, LOW);
+
+  //Publish a message if the panel starts or stops charging the battery
+  if (panel.read())
+  {
+    bool charging =
+        panel.current() >= CHARGE_CURRENT_LOW_THRESHOLD && panel.current() <= CHARGE_CURRENT_HIGH_THRESHOLD;
+    if (charging && !panelOn)
+    {
+      publishStatusMessage("PANEL ON");
+    }
+    if (!charging)
+    {
+      publishStatusMessage("PANEL OFF");
+      if (settings.panelOffMinutes > 0)
+      {
+        deepSleep(settings.panelOffMinutes * 60);
+      }
+    }
+    panelOn = charging;
+  }
+
+  //sleep till the next reading
+  void (*sleepAction)();
+  const char *sleepMessage;
+  if (settings.useDeepSleep)
+  {
+    sleepMessage = "DEEP";
+    sleepAction = []() {
+      deepSleep(settings.sleepTime);
+    };
+  }
+  else
+  {
+    sleepMessage = "LIGHT";
+    sleepAction = []() {
+      delay(settings.sleepTime);
+      setup();
+    };
+  }
+
+  Serial.printlnf("%s SLEEP %d", sleepMessage, settings.sleepTime);
+  Serial.printlnf("DURATION %d", millis() - duration);
+
+  Particle.process();
+  Serial.flush();
+
+  sleepAction();
 }
--- DeviceFirmware/ParticleBoron/src/Sensor.h
+++ DeviceFirmware/ParticleBoron/src/Sensor.h
@@ -1,12 +1,154 @@
 #ifndef SENSOR_h
 #define SENSOR_h
 
-class Sensor {
-  public:
-    virtual int getReadingSize();
-    virtual bool getReading(char* reading);
+class Sensor
+{
+public:
+    const char *Name;
+    const int ReadingSize;
+    Sensor(int readingSize, const char *name) : ReadingSize(readingSize)
+    {
+        Name = name;
+    }
+    virtual bool getReading(char *reading);
     virtual bool begin();
 };
 
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <LaCrosse_TX23.h>
+#include <Adafruit_INA219.h>
+#include <Compass.h>
+#include <math.h>
+
+class CompassSensor : public Sensor
+{
+private:
+    Compass compassSensor;
+
+public:
+    CompassSensor() : Sensor(12, "compass") {}
+    bool getReading(char *buffer)
+    {
+        CompassReading reading = compassSensor.getReading();
+        buffer += sprintf(buffer, "c%3.0f:%3.0f:%3.0f", reading.x, reading.y, reading.z);
+        return true;
+    }
+    bool begin()
+    {
+        return compassSensor.begin();
+    }
+};
+
+class BatteryPower : public Sensor
+{
+private:
+    FuelGauge fuelGuage;
+
+public:
+    BatteryPower() : Sensor(14, "battery") {}
+    bool getReading(char *reading)
+    {
+        float batteryVoltage = fuelGuage.getVCell();
+        float batteryPercentage = fuelGuage.getSoC();
+        reading += sprintf(reading, "p%9.6f:%3.2f", batteryVoltage, batteryPercentage);
+        return true;
+    }
+    bool begin()
+    {
+        return true;
+    }
+};
+
+class PanelPower : public Sensor
+{
+private:
+    Adafruit_INA219 powerMonitor;
+    bool _read;
+    float _voltage;
+    float _current;
+
+public:
+    PanelPower() : Sensor(21, "panel") {}
+    bool getReading(char *reading)
+    {
+        _voltage = powerMonitor.getBusVoltage_V();
+        _current = powerMonitor.getCurrent_mA();
+        _read = _voltage < 16;
+        if (_read)
+        {
+            reading += sprintf(reading, "p%9.6f:%9.6f", _voltage, _current);
+        }
+        return _read;
+    }
+    bool begin()
+    {
+        powerMonitor.begin();
+        powerMonitor.setCalibration_16V_400mA();
+        return true;
+    }
+    bool read() { return _read; }
+    float voltage() { return _voltage; }
+    float current() { return _current; }
+};
+
+class Anemometer : public Sensor
+{
+private:
+    LaCrosse_TX23 laCrosseTX23;
+    int maxTries;
+
+public:
+    Anemometer(const int pin, const int maxTries) : Sensor(13, "anemometer"), laCrosseTX23(pin)
+    {
+        this->maxTries = maxTries;
+    }
+    int getReadingSize()
+    {
+        return 13;
+    }
+    bool getReading(char *reading)
+    {
+        bool read = false;
+        int tries = 0;
+        float speed = 0.0;
+        int direction = 0;
+        while (!(read = laCrosseTX23.read(speed, direction)) && tries++ < maxTries)
+            ;
+        if (read)
+        {
+            reading += sprintf(reading, "a%9.6f:%2d", speed, direction);
+        }
+        return read;
+    }
+    bool begin()
+    {
+        float speed;
+        int direction;
+        return laCrosseTX23.read(speed, direction);
+    }
+};
+
+class Barometer : public Sensor
+{
+private:
+    Adafruit_BME280 bme280;
+
+public:
+    Barometer() : Sensor(24, "barometer") {}
+    bool getReading(char *reading)
+    {
+        float bmeTemperature = bme280.readTemperature();
+        float pressure = bme280.readPressure();
+        float bmeHumidity = bme280.readHumidity();
+        bool result = !isnan(bmeTemperature) && !isnan(pressure) && pressure > 0.0 && !isnan(bmeHumidity);
+        reading += sprintf(reading, "b%9.6f:%9.6f:%9.6f", bmeTemperature, pressure, bmeHumidity);
+        return result;
+    }
+    bool begin()
+    {
+        return bme280.begin(0x76);
+    }
+};
 
 #endif
