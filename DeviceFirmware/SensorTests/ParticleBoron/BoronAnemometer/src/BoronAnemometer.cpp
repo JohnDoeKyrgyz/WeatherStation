42c40722671ec0100f6776ab32552043a9fb4c0d
John Atwood <JohnDoeKyrgyz@gmail.com>
2020-03-30 19:55:03 -0400

    Remove enable pin

--- DeviceFirmware/SensorTests/ParticleBoron/BoronAnemometer/src/BoronAnemometer.cpp
+++ DeviceFirmware/SensorTests/ParticleBoron/BoronAnemometer/src/BoronAnemometer.cpp
@@ -2,7 +2,7 @@
 //       THIS IS A GENERATED FILE - DO NOT EDIT       //
 /******************************************************/
 
-#include "application.h"
+#include "Particle.h"
 #line 1 "c:/working/WeatherStation/DeviceFirmware/SensorTests/ParticleBoron/BoronAnemometer/src/BoronAnemometer.ino"
 void setup();
 void loop();
@@ -19,18 +19,13 @@ LaCrosse_TX23 laCrosseTX23(ANEMOMETER);
 void setup() {
   Serial.begin(115200);
   Serial.println("Anemometer test");
-
-  pinMode(D6, OUTPUT);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
-  
-  digitalWrite(D6, HIGH);
   float windSpeed;
   int windDirection;
   bool result = laCrosseTX23.read(windSpeed, windDirection);
 
   Serial.printlnf("%d, %f, %d", result, windSpeed, windDirection);
-  digitalWrite(D6, LOW);
 }
