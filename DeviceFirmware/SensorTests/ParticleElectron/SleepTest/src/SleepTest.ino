7122fa042d4de43c3893dd2c7ff2608026e8a3be
John Atwood <JohnDoeKyrgyz@gmail.com>
2020-03-30 19:51:41 -0400

    Adding a SleepTest app for Electron

---
+++ DeviceFirmware/SensorTests/ParticleElectron/SleepTest/src/SleepTest.ino
@@ -0,0 +1,27 @@
+FuelGauge fuel;
+void setup()
+{
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  char buffer[100];
+  snprintf(buffer, sizeof buffer, "Voltage: %f, SoC %f, NormalizedSoc %f", fuel.getVCell(), fuel.getSoC(), fuel.getNormalizedSoC());
+  Serial.println(buffer);
+
+  Particle.publish("READING", buffer, PRIVATE);
+
+  //deep sleep
+  #if PLATFORM_ID == 10 //ELECTRON  
+  SystemSleepConfiguration config;
+  auto sleepDuration = 10s;
+  config
+    .mode(SystemSleepMode::HIBERNATE)
+    .gpio(WKP, RISING)
+    .duration(sleepDuration);    
+  System.sleep(config);
+  #elif PLATFORM_ID == 13 //BORON
+  System.sleep({}, RISING, SLEEP_NETWORK_STANDBY, 10);
+  #endif
+}
